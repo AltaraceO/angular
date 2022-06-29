37a6963b0adbf4a623c37552131344dc990e7a9a
AltaraceO <orialtarace@gmail.com>
2022-06-29 14:58:08 +0300

    create a new component

---
+++ first-app/src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+// component decorator is there to enhance a class
+//js object tells angular what to do with this
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {}
