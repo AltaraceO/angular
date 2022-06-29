254cb37d396d44790eb6c7b271b6ef3cad723258
AltaraceO <orialtarace@gmail.com>
2022-06-29 15:03:59 +0300

    decorator and registering new components

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  //registering new components here so that angular is aware of them.
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
