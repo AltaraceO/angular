5f2eb83a1d6f79b8316dd9b18feadef491494fef
AltaraceO <orialtarace@gmail.com>
2022-07-02 11:59:15 +0300

    property/event binding

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server created';
+
+  serverId = 10;
+  serverStatus = 'offline';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+}
